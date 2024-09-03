1408c78b1a2b15c34a8e195754421cd01bf7ec06
--add <priyakadam0018@gmail.com>
2024-09-03 17:21:02 +0530

    code updated

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const age = 25;
 const name = "priya";
 const dob ="16/09/1999";
-const isadult = () => { return age >= 28;}
+const isadult = () => { return age >= 28; }
+function myfunc() {
+    console.log("Hello world")
+};
