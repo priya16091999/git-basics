ebe7a2b5957318fcfdaea9f6658d600b134f677d
--add <priyakadam0018@gmail.com>
2024-09-03 18:22:01 +0530

    commit msg

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const age = 25;
 const name = "priya";
 const dob ="16/09/1999";
-const isadult = () => { return age >= 28; }
-function myfunc() {
-    console.log("Hello world")
-};
+const isadult = () => { return age >= 28;}
